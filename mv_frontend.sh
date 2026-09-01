f7c343aed7a62c94bee357ec878a0031f5299408
Cesur86 <kravenos@gmail.com>
2026-09-01 17:54:42 +0200

    chore: remove obsolete repository files

--- mv_frontend.sh
+++
@@ -1,1 +0,0 @@
-rsync -av frontend/rakuyomi.koplugin/ ~/.config/koreader/plugins/rakuyomi.koplugin/
--- mv_linux.sh
+++
@@ -1,8 +0,0 @@
-
-cd backend
-cargo build
-
-cd ..
-cp backend/target/debug/uds_http_request ~/.config/koreader/plugins/raku*/
-cp backend/target/debug/server ~/.config/koreader/plugins/raku*/
-cp backend/target/debug/cbz_metadata_reader ~/.config/koreader/plugins/raku*/
